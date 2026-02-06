b7e09fbd479bf41daa2f4ae7198f2e0642aed069
estefanbranisla <267591+estefanbranisla@users.noreply.github.com>
2026-02-06 23:16:38 +0100

    Change python script

--- wasm.sh
+++ wasm.sh
@@ -11,4 +11,9 @@ PORT="${1:-8080}"
 echo "Serving benchmark UI at http://localhost:$PORT"
 echo "Press Ctrl+C to stop"
 
-cd "$SCRIPT_DIR/ui" && python3 -m http.server "$PORT"
+cd "$SCRIPT_DIR/ui" && python3 -c "
+import http.server, socketserver
+socketserver.TCPServer.allow_reuse_address = True
+with socketserver.TCPServer(('', $PORT), http.server.SimpleHTTPRequestHandler) as httpd:
+    httpd.serve_forever()
+"
